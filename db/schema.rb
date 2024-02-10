230eb750af43c291aa3d6ab454562ed9268804c4
Rashad DuPaty <radupaty@gmail.com>
2024-02-10 16:45:06 +0900

    user model complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_10_070732) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_10_073834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_10_070732) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "learning_style"
+    t.boolean "teacher"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
