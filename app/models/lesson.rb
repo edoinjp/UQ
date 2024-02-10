151d92ee850e4fe2f1c5dbaf4c897d7fbc4eff88
Rashad DuPaty <radupaty@gmail.com>
2024-02-10 17:27:41 +0900

    added associations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ApplicationRecord
   belongs_to :classroom
+  has_many :questions
+  has_many :styled_lessons
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :classrooms, dependent: :destroy
+  has_many :classrooms_as_student, through: :participations, source: :classroom
 end
