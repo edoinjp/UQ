204a9673c3825652782b1caf0e9fa854d330a295
Osama Suleiman <osama.suleiman97@gmail.com>
2024-02-13 21:46:22 +0900

    made show method and added user policy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show]
+  after_action :verify_authorized
+
+  def show
+    authorize @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
