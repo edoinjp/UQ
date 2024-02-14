1785d6ce12a85349ec93bb82501e8ad2fbddfedb
Rashad DuPaty <radupaty@gmail.com>
2024-02-14 22:33:07 +0900

    testing seeds

--- app/policies/lesson_policy.rb
+++ app/policies/lesson_policy.rb
@@ -1,7 +1,6 @@
 class LessonPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-<<<<<<< HEAD
     def resolve
       scope.all # the same as "Lesson.all"
     end
@@ -9,10 +8,5 @@ class LessonPolicy < ApplicationPolicy
 
   def show?
     true # switch from application policy so everyone can see the page
-=======
-    # def resolve
-    #   scope.all
-    # end
->>>>>>> master
   end
 end
