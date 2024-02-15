d50f832eb8bd564b508d63c4bc21d4a2f583ed8a
eduard gurchiani <edoinjp@gmail.com>
2024-02-15 17:03:00 +0900

    created main view for all the in class pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#home"  # Root path for the home action in HomeController
 
-  resources :classrooms, only: %i[index new create show]
+  resources :classrooms, only: %i[index new create show] do
+    member do
+      post 'add_students'
+    end
+  end
+
   resources :users, only: [:show]
   resources :lessons, only: %i[index show] do
     resources :questions, only: [:index]
   end
 
-  resources :classrooms do
-    member do
-      post 'add_students'
-    end
-  end
 end
