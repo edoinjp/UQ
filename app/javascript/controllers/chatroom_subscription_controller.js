280a34d447431c13a1f6cdf978ded2a76fe77c95
eduard gurchiani <edoinjp@gmail.com>
2024-02-24 14:52:12 +0900

    added anonymus unless user is not teacher user cant see name in chat debug scroll removed students names from chat to dont display it anymore

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -25,7 +25,6 @@ export default class extends Controller {
 
     // Get the last message element
     const lastMessage = this.messagesTarget.lastElementChild;
-
     // Scroll the last message into view
     lastMessage.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
