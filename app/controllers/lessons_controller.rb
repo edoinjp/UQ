8c4b2003d83b16852279cdee4617142ac4e6ee95
Rashad DuPaty <radupaty@gmail.com>
2024-02-29 20:05:46 +0900

    Added AI test to the lessons show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -27,6 +27,7 @@ class LessonsController < ApplicationController
     if params[:query].present?
       @students = @students.where(learning_style: params[:query])
     end
+
     # @openai_api = OpenaiApi.new
     # @paragraph = @openai_api.generate_content(@lesson.title)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_24_052818) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_29_051833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_24_052818) do
     t.string "learning_style"
     t.boolean "teacher"
     t.string "nickname"
+    t.json "scores", default: {}
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
