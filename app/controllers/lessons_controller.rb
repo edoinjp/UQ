90eef3e487e2edbdb6cb906bda82b22aee0947e1
Rashad DuPaty <radupaty@gmail.com>
2024-03-01 09:53:49 +0900

    finished first step towards AI help chat

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_classroom, only: [:index, :new, :create]
   before_action :set_lesson, only: [:show]
+  include ActionController::Live # Allows AI chat streaming
 
   def index
     @lessons = policy_scope(Lesson)
@@ -27,6 +28,37 @@ class LessonsController < ApplicationController
     if params[:query].present?
       @students = @students.where(learning_style: params[:query])
     end
+
+    # AI Chat Helper Code
+    response.headers['Content-Type'] = 'text/event-stream'
+    response.headers['Last-Modified'] = Time.now.httpdate # Helps prevent bug from streaming output
+    sse = SSE.new(response.stream, event: 'meessage') # Variable for Server Sent Event
+    client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
+
+    begin
+      client.chat( # Calls the chat API
+        parameters: {
+          model: 'gpt-3.5-turbo',
+          messages: [{ role: 'user', content: params[:prompt] }], # Prompt to pass as a query parameter
+          stream: proc do |chunk| # Creates a procedure to handle incoming steam from API
+            content = chunk.dig('choices', 0, 'delta', 'content') # Fetches content from API response
+            return if content.nil? # Return from procedure once theres no more content
+            see.write(object{ message: content }) # Writes the response to the message event
+          end
+        }
+      )
+    ensure
+      see.close # Makes sure it closes
+    end
+
+
+
+
+
+
+
+
+
 
     # @openai_api = OpenaiApi.new
     # @paragraph = @openai_api.generate_content(@lesson.title)
