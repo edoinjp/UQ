35488f75fc1c035ec1010cd22443e2060f34e412
eduard gurchiani <edoinjp@gmail.com>
2024-03-04 00:32:28 +0900

    changed chat styling added colors to senders and recivers

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,4 +1,3 @@
-
 import { Controller } from "@hotwired/stimulus";
 import { createConsumer } from "@rails/actioncable";
 
@@ -8,17 +7,34 @@ export default class extends Controller {
 
   connect() {
     console.log("Connected to the chatroom subscription controller.");
-    this.channel = createConsumer().subscriptions.create(
+
+    // Connect to ChatroomChannel for scrolling
+    this.channelWithScroll = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       {
         received: data => this.insertMessageAndScrollDown(data),
       }
     );
+
+    // Connect to ChatroomChannel for additional logic
+    this.channelWithAdditionalLogic = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      {
+        received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data),
+      }
+    );
+
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`);
   }
 
   disconnect() {
     console.log("Disconnected from the chatroom subscription controller.");
-    this.channel.unsubscribe();
+
+    // Disconnect from ChatroomChannel for scrolling
+    this.channelWithScroll.unsubscribe();
+
+    // Disconnect from ChatroomChannel for additional logic
+    this.channelWithAdditionalLogic.unsubscribe();
   }
 
   insertMessageAndScrollDown(data) {
@@ -26,15 +42,4 @@ export default class extends Controller {
     const lastMessage = this.messagesTarget.lastElementChild;
     lastMessage.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
-
-  connect() {
-    this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) }
-    )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
-  }
-
-
-
 }
