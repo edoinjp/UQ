2da752a0dee8e262a454eaa03881ae6d1b165370
Osama Suleiman <osama.suleiman97@gmail.com>
2024-03-05 00:50:19 +0900

    editing minor designs

--- app/javascript/controllers/lesson_controller.js
+++ app/javascript/controllers/lesson_controller.js
@@ -1,10 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "stimulus"
 
-// Connects to data-controller="lesson"
 export default class extends Controller {
   static targets = ["mainContent", "supplementaryContent"]
-  toggleSupplementaryContent() {
-    this.mainContentTarget.classList.toggle("d-none");
-    this.supplementaryContentTarget.classlist.toggle("d-none");
+
+  toggleSupplementaryContent(event) {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      this.mainContentTarget.classList.add("d-none");
+      this.supplementaryContentTarget.classList.remove("d-none");
+    } else {
+      this.mainContentTarget.classList.remove("d-none");
+      this.supplementaryContentTarget.classList.add("d-none");
+    }
   }
 }
