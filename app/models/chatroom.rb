db3c22bc9d51a48090d1e2f0d81b275ec6dbe70d
eduard gurchiani <edoinjp@gmail.com>
2024-03-04 23:45:29 +0900

    Clear previous records was in wrong order that was cause to net being able to seed

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
   belongs_to :classroom
-  has_many :messages
-  has_many :direct_messages
+  has_many :messages, dependent: :destroy
+  # has_many :direct_messages, dependent: :destroy
   has_and_belongs_to_many :users
 end
