91fe7ab3f996efe24dfa75f51527f2fe1c4bb72c
Rashad DuPaty <radupaty@gmail.com>
2024-03-04 21:20:04 +0900

    Deleted previous code and confirmed functionality

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -12,45 +12,7 @@ class ResponsesController < ApplicationController
     # @responses = policy_scope(Response.includes(:choice).where(choices: { question_id: @lesson.questions.ids }))
 
     # Get all students who participated in the lesson
-    students = @lesson.classroom.students
-
-    # Initialize @student_scores as an empty hash
-    @student_scores = {}
-
-    # For each student, fetch the correct answer count from the database
-    students.each do |student|
-      lesson_score = student.score[@lesson.title] # Assuming 'title' is the lesson title
-      if lesson_score.present?
-        @student_scores[student] = {
-          correct_count: lesson_score, # Assign the score value directly
-          # total_questions: @lesson.questions.count Use this later when 5 total questions are available
-          total_questions: 5
-        }
-      else
-        # Handle the case where the score for the lesson is not present for the student
-        @student_scores[student] = {
-          correct_count: 0,
-          # total_questions: @lesson.questions.count Use this later when 5 total questions are available
-          total_questions: 5
-        }
-      end
-    end
-
-    # Creates quiz scores for each seeded lesson
-    @lessons_with_scores = [@lesson.classroom].map(&:lessons).flatten.map do |lesson|
-      { lesson: lesson, quiz_score: rand(0..5) }
-    end
-
-    # Creates additional lessons on top of the seeded ones
-    additional_lesson_titles = ['Oral Communication II', 'Social Science', 'Language Arts']
-    additional_lesson_titles.each do |title|
-      @lessons_with_scores << { lesson: OpenStruct.new(title: title), quiz_score: rand(0..5) }
-    end
-    # Creates the individual student progress charts in the pop-up
-    @chart_data = {}
-    @lessons_with_scores.each do |lesson_result|
-      @chart_data[lesson_result[:lesson].title] = lesson_result[:quiz_score]
-    end
+    @students = @lesson.classroom.students
 
     # Creates the class response score totals
     visual_l1_total = 0
@@ -79,7 +41,7 @@ class ResponsesController < ApplicationController
     kinesthetic_student_total = 0
 
 
-    students.each do |student|
+    @students.each do |student|
 
       # Lesson 1 Totals Filtered by Style
       if student.learning_style == 'visual'
