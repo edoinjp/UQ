838fa8c12ad45ed6e0b605a5366cf0fca766e7c6
Osama Suleiman <osama.suleiman97@gmail.com>
2024-03-05 19:06:25 +0900

    adjusted chatroom model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,5 +2,8 @@ class Chatroom < ApplicationRecord
   belongs_to :classroom
   has_many :messages, dependent: :destroy
   # has_many :direct_messages, dependent: :destroy
-  has_and_belongs_to_many :users
+  # has_and_belongs_to_many :users
+  has_many :chatroom_users
+  has_many :chatroom, through: :chatroom_users
+  has_many :users, through: :chatroom_users
 end
