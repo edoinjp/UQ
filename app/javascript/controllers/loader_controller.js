9aaf1f8a8e35cb7ac3f47c8fc4ca84ffaa7fb7b2
Rashad DuPaty <radupaty@gmail.com>
2024-03-08 13:14:14 +0900

    added css spinner

---
+++ app/javascript/controllers/loader_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="loader"
+export default class extends Controller {
+  connect() {
+    console.log('Sup')
+  }
+
+  spinner(event) {
+    
+  }
+}
